ab99ce893ec7acd9cb872a892f4d7cc1e9333de5
marioguuerra2002 <alu0101395036@ull.edu.es>
2023-11-26 21:38:51 +0100

    exercise3 finish

--- src/exercise3/exercise3.py
+++ src/exercise3/exercise3.py
@@ -5,6 +5,7 @@ def encryptSubstitution(key, text):
   for i in range(len(key)):
     sum_key += ord(key[i])
   sum_key = sum_key % 255
+  print ("The sum of the key is: ", sum_key)
   for i in range(len(text)):
     if text[i] == " ":
       encrypted_text += " "
@@ -18,6 +19,7 @@ def encryptSubstitution(key, text):
   return encrypted_text
 def decryptSubstitution(key, text):
   decrypted_text = "" # Initialize the decrypted text
+  print ("The key is: ", ord(key))
   sum_key = 0
   for i in range(len(key)):
     sum_key += ord(key[i])
@@ -32,7 +34,47 @@ def decryptSubstitution(key, text):
       auxiliar = (auxiliar - sum_key) % 255
       auxiliar2 = chr(auxiliar) # Convert the number to letter
       decrypted_text += auxiliar2
+    
   return decrypted_text
+def isMeaningful(text_entry):
+    name_file = "text_to_compare.txt"
+    path_file = name_file
+    # Read the comparator text (the square)
+    with open(path_file, 'r') as f:
+        text_to_compare = f.read()
+    # print ("The text to compare is: ", text_to_compare)
+    
+    # Read the text to compare from the commun file
+    text_check = text_entry
+    print ("The text to check is: ")
+    print (text_check)
+    
+    # counter = 0
+    # for i in range(len(text_check)):
+    #   while counter < 7: # take the first 7 lines
+    #     while text_check[i] != "\n":
+    #       seven_first_lines += text_check[i]
+    #       counter += 1
+    text_check = text_check.split("\n")
+    seven_first_lines = ""
+    for i in range(7):
+      seven_first_lines += text_check[i]
+      seven_first_lines += "\n"
+    
+    print ("The text to compare is: ", seven_first_lines)
+    if text_to_compare == seven_first_lines:
+        return True
+    else:
+        return False
+      
+def decryptAndCheckMeaning(text):
+    for key in range(256):
+        decrypted_text = decryptSubstitution(chr(key), text)
+        if isMeaningful(decrypted_text):
+          print ("The key is: ", chr(key))
+          return decrypted_text
+          
+    return None
 def encryptTransposition(key, text):
   num_columns = len(key)
   
@@ -116,7 +158,7 @@ def main ():
   method = input ("Enter the method you want to use(substitution or transposition): ")
   operation = input ("Enter the operation you want to use(encrypt or decrypt): ")
   key=""
-  key = input ("Enter the key you want to use: ")
+  
   
   input_file = sys.argv[1]
   
@@ -124,16 +166,20 @@ def main ():
     text = f.read() # Read the file
   if method == "substitution":
     if operation == "encrypt":
+      key = input ("Enter the key you want to use: ")
       procesed_text = encryptSubstitution(key, text)
     elif operation == "decrypt":
-      procesed_text = decryptSubstitution(key, text)
+      procesed_text = decryptAndCheckMeaning(text)
+      
     else:
       print("Invalid operation")
       return
   elif method == "transposition":
     if operation == "encrypt":
+      key = input ("Enter the key you want to use: ")
       procesed_text = encryptTransposition(key, text)
     elif operation == "decrypt":
+      key = input ("Enter the key you want to use: ")
       procesed_text = decryptTransposition(key, text)
     else:
       print("Invalid operation")
@@ -145,6 +191,9 @@ def main ():
     out_text = input_file + ".enc"
   elif operation == "decrypt":
     out_text = input_file + ".dec"
+  if (procesed_text == None):
+    print("The text could not be decrypted")
+    return
   with open(out_text, 'w') as file:
     file.write(procesed_text)
   print("File processed successfully. Saved as", out_text)
